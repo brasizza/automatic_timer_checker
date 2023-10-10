9fce83c1c16243da2e8c62d05962afa89af2819e
Marcus Vinicius Brasizza <mvbdesenvolvimento@gmail.com>
2023-10-10 12:06:25 -0300

    final

--- ios/Classes/AutomaticTimerCheckerPlugin.swift
+++ ios/Classes/AutomaticTimerCheckerPlugin.swift
@@ -1,5 +1,6 @@
 import Flutter
 import UIKit
+import Foundation
 
 public class AutomaticTimerCheckerPlugin: NSObject, FlutterPlugin {
   public static func register(with registrar: FlutterPluginRegistrar) {
